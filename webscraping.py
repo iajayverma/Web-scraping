b4921843d6174252a84c8f00cd5c2c0388f627fb
Ajay Verma <aviverma.cse@gmail.com>
2024-07-11 18:53:12 +0530

    modified

--- webscraping.py
+++ webscraping.py
@@ -30,46 +30,51 @@ st.markdown("""
 
 st.markdown("<h1 style='text-align:center'>Web Scraper</h1>", unsafe_allow_html=True)
 
+# Form for keyword input
 with st.form("search"):
-    col1,col2,col3,col4=st.columns(4)
+    col1, col2, col3, col4 = st.columns(4)
     keyword = col2.text_input("Enter your keyword")
-    
     search = col3.form_submit_button("search")
+
     if search:
-        mynewkey=len(keyword.split(' '))
-       
-        if mynewkey >1:
-            keywordwords=keyword.replace(' ','-')
-            
-        else:
-            keywordwords=keyword
+        if keyword:
+            # Process the keyword for URL
+            keywordwords = keyword.replace(' ', '-') if len(keyword.split(' ')) > 1 else keyword
 
-        page = requests.get(f"https://unsplash.com/s/photos/{keywordwords}")
-       
-        soup = BeautifulSoup(page.content, 'html.parser')
-        # Select all img tags within the div with class 'HcSeS'
-        imgs = soup.select('div.HcSeS img')
-        data = []
-        for img in imgs:
-            srcset = img.get('srcset')
-            if isinstance(srcset, str): 
-                if '?' in srcset:  
-                    myindex = srcset.index('?')
-                    myimage = srcset[:myindex]
-                    data.append({'srcset': srcset, 'myindex': myindex, 'myimage': myimage})
+            # Fetch the webpage
+            page = requests.get(f"https://unsplash.com/s/photos/{keywordwords}")
+            if page.status_code == 200:
+                # Parse the content with BeautifulSoup
+                soup = BeautifulSoup(page.content, 'html.parser')
 
-        if data:
-            df = pd.DataFrame(data)
-            cols = st.columns(3) 
-            for index, row in df.iterrows():
-                col = cols[index % 3]
-                col.markdown(
-                    f"""
-                    <a href="{row['myimage']}" target="_blank">
-                        <img src="{row['myimage']}" width="100%">
-                    </a>
-                    """,
-                    unsafe_allow_html=True
-                )
+                # Select all img tags within the div with class 'HcSeS'
+                imgs = soup.select('div.HcSeS img')
+                data = []
+
+                for img in imgs:
+                    srcset = img.get('srcset')
+                    if isinstance(srcset, str): 
+                        if '?' in srcset:  
+                            myindex = srcset.index('?')
+                            myimage = srcset[:myindex]
+                            data.append({'srcset': srcset, 'myindex': myindex, 'myimage': myimage})
+
+                if data:
+                    df = pd.DataFrame(data)
+                    cols = st.columns(3) 
+                    for index, row in df.iterrows():
+                        col = cols[index % 3]
+                        col.markdown(
+                            f"""
+                            <a href="{row['myimage']}" target="_blank">
+                                <img src="{row['myimage']}" width="100%">
+                            </a>
+                            """,
+                            unsafe_allow_html=True
+                        )
+                else:
+                    st.write("No images found.")
+            else:
+                st.error("Failed to retrieve content from Unsplash.")
         else:
-            st.write("No images found.")
+            st.error("Please enter a keyword to search.")
